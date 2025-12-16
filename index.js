3301c3e9e3ae7fef4f462224093dea4b2c145c93
tulasiprasadk <tulasiprasadk@gmail.com>
2025-12-17 03:46:22 +0530

    Remove all files to prepare for new backend

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ const allowedOrigins = (process.env.CORS_ORIGINS
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "http://localhost:4173",
+    "http://127.0.0.1:4173",
 
     // Production frontend (GitHub Pages)
     "https://tulasiprasadk.github.io",
